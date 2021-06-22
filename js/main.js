d22da0c9468d6de6045b7818fe9108056423ffcd
Kate <eva0913@yandex.ru>
2021-06-22 11:42:43 +0300

    задание 2, исправила ошибки оформления линтером

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 const getRandom = (min, max, signsCount) => {
-    if (min<0 || max<0){
-        return 0;
-    }
-    if ( min > max ) {
-        let tmp = min;
-        min = max;
-        max = tmp;
-    }
-    const num = Math.random() * (max - min) + min;
-    return num.toFixed(signsCount);
+  if (min<0 || max<0){
+    return 0;
+  }
+  if ( min > max ) {
+    const tmp = min;
+    min = max;
+    max = tmp;
+  }
+  const num = Math.random() * (max - min) + min;
+  return num.toFixed(signsCount);
 };
 getRandom(0,0.3,1);
