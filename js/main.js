e35fae1b3b96022bc60af5483fc6db2f61c730ef
Kate <eva0913@yandex.ru>
2021-08-08 10:33:35 +0300

    задание 4, добавила генерацию тестовых объектов

--- js/main.js
+++ js/main.js
@@ -1,13 +1,102 @@
-const getRandom = (min, max, signsCount) => {
-  if (min<0 || max<0){
-    return 0;
-  }
-  if ( min > max ) {
-    const tmp = min;
-    min = max;
-    max = tmp;
-  }
-  const num = Math.random() * (max - min) + min;
-  return num.toFixed(signsCount);
+const SIMILAR_AD_COUNT = 10;
+const TITLES = [
+  'Невероятно горячее предложение',
+  'Бесплатная отмена бронирования',
+  'Страховой залог и предоплата не требуется',
+  'Лучшее предложение этого месяца',
+];
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+const CHECK_PERIODS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const DESCRIPTIONS = [
+  'Кондиционер и сейф для личных вещей',
+  'Розетки около кровати',
+  'Звукоизоляция во всем помещении',
+  'C видом на живописный парк',
+];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const getRandomInt = function(min, max) {
+  const rand = Math.random() * (max - min + 1) + min;
+  return Math.abs(Math.round(rand));
 };
-getRandom(0,0.3,1);
+
+const getRandomFloat = function(min, max, fix){
+  const randomNum = (Math.random() * (max - min) + min).toFixed(fix);
+  return Math.abs(randomNum);
+};
+
+const getRandomElement = function(arr) {
+  const randomElement = Math.floor(Math.random() * arr.length);
+  return arr[randomElement];
+};
+
+let numberForAvatar = 0;
+const getUniqueAvatarSrc = function() {
+  for (let i = 1; i <= SIMILAR_AD_COUNT; i++) {
+    numberForAvatar += 1;
+    if (numberForAvatar < 10) {
+      return`img/avatars/user0${numberForAvatar}.png`;
+    }
+    return`img/avatars/user${numberForAvatar}.png`;
+  }
+};
+
+const getRandomArray = (array) => {
+  const randomArray = new Array (getRandomInt(1, array.length)).fill(' ').map(() => (getRandomElement(array)));
+  const uniqueElementsArray = [...new Set(randomArray)];
+  return uniqueElementsArray;
+};
+
+const getAdvt = function() {
+  const x = getRandomFloat(35.65000, 35.70000, 5);
+  const y = getRandomFloat(139.7000, 139.80000, 5);
+
+  return {
+    author: {
+      avatar: getUniqueAvatarSrc(),
+    },
+    offer: {
+      title: getRandomElement(TITLES),
+      address: `${x}, ${y}`,
+      price: getRandomInt(1000, 100000),
+      type: getRandomElement(TYPES),
+      rooms: getRandomInt(1, 10),
+      guests: getRandomInt(1, 20),
+      checkin: getRandomElement(CHECK_PERIODS),
+      checkout: getRandomElement(CHECK_PERIODS),
+      features: getRandomArray(FEATURES),
+      description: getRandomElement(DESCRIPTIONS),
+      photos: getRandomArray(PHOTOS),
+    },
+    location: {
+      lat: x,
+      lng: y,
+    }
+  };
+};
+
+const arrOfAdvt = new Array(SIMILAR_AD_COUNT).fill(null).map(() => getAdvt());
+
+console.log(arrOfAdvt);
